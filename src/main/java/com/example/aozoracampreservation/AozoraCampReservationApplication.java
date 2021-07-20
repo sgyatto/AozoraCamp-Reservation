f88eed83a83d5f6651621988452016a4eb4f35dc
sgyatto <42696711+sgyatto@users.noreply.github.com>
2021-07-20 21:53:36 +0900

    Initial commit

---
+++ src/main/java/com/example/aozoracampreservation/AozoraCampReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.aozoracampreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AozoraCampReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AozoraCampReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aozoracampreservation/AozoraCampReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aozoracampreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AozoraCampReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
