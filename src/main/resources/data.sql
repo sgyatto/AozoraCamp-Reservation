7aa264d0e724f7ab84f4a2edc1291629c6c56dc7
sgyatto <42696711+sgyatto@users.noreply.github.com>
2021-08-16 12:14:33 +0900

    DBマイグレーション時のデータ内容変更とシーケンスがリセットされるよう変更

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-delete from reservation_details;
-delete from reservations;
-delete from site_availability;
-delete from calendar;
-delete from site_rates;
-delete from rate_types;
-delete from sales_tax;
-delete from site_types;
-delete from members;
-delete from roles;
+truncate reservation_details restart identity cascade;
+truncate reservations restart identity cascade;
+truncate site_availability restart identity cascade;
+truncate calendar restart identity cascade;
+truncate site_rates restart identity cascade;
+truncate rate_types restart identity cascade;
+truncate sales_tax restart identity cascade;
+truncate site_types restart identity cascade;
+truncate members restart identity cascade;
+truncate roles restart identity cascade;
 
 -- role
 insert into roles (
@@ -29,8 +29,8 @@ insert into members (
                     , updated_at
                     , created_at
 ) values
-(1, '藤村忠寿', 'aozora@camp.com', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', '09012345678', 'ROLE_GENERAL', default, default)
-,(2,'嬉野雅道','aozora2@camp.com','$2a$10$bWrKNHFJ4bDoUl/hSt6GoeKm1QHdAkkrFDV.Lh5SKEumpg9kAZ8TO','09088888888','ROLE_GENERAL',default, default)
+(default , '藤村忠寿', 'aozora@camp.com', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', '09012345678', 'ROLE_GENERAL', default, default)
+,(default ,'嬉野雅道','aozora2@camp.com','$2a$10$bWrKNHFJ4bDoUl/hSt6GoeKm1QHdAkkrFDV.Lh5SKEumpg9kAZ8TO','09088888888','ROLE_GENERAL',default, default)
 ;
 
 -- site_types
@@ -144,27 +144,29 @@ from
     generate_series('2021-08-01'::date, '2022-12-31', '1 day')
 ;
 
+-- reservations
+insert into "reservations"("id","site_type_id","date_from","stay_days","number_of_people","total_amount_tax_incl","sales_tax","reservation_method","member_id","non_member_name","non_member_mail","non_member_phone_number","canceled_at","created_at")
+values
+(default ,1,E'2021-09-02',2,1,8800,800,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-15 13:45:31.934168'),
+(default ,3,E'2021-08-25',1,1,7700,700,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-15 13:46:07.733142'),
+(default ,3,E'2021-08-31',1,1,7700,700,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-15 13:46:33.467437'),
+(default ,2,E'2021-09-15',3,3,18150,1650,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-15 13:48:15.109568'),
+(default ,3,E'2021-09-13',1,1,7700,700,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-15 13:48:50.726346'),
+(default ,2,E'2021-09-18',2,1,12100,1100,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-15 13:49:31.789736'),
+(default ,3,E'2021-08-28',1,1,7700,700,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-15 13:50:34.095119');
 
-INSERT INTO "reservations"("id","site_type_id","date_from","stay_days","number_of_people","total_amount_tax_incl","sales_tax","reservation_method","member_id","non_member_name","non_member_mail","non_member_phone_number","canceled_at","created_at")
+
+-- reservation_details
+INSERT INTO "reservation_details"("reservation_id","reservation_date","site_rate","tax_rate","rate_type_name","created_at")
 VALUES
-(9,1,E'2021-08-31',2,2,9900,900,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-10 00:02:39.238429'),
-(10,2,E'2021-09-03',1,1,5500,500,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-10 00:02:53.467315'),
-(11,3,E'2021-08-31',2,1,16500,1500,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-10 00:03:18.611791'),
-(12,1,E'2021-09-09',1,1,3300,300,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-10 00:03:28.049619'),
-(13,2,E'2021-08-12',1,1,8800,800,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-10 00:03:56.011949'),
-(14,3,E'2021-08-13',1,1,9900,900,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-10 00:04:04.620321'),
-(15,3,E'2021-09-09',2,2,13200,1200,E'1',1,NULL,NULL,NULL,NULL,E'2021-08-10 00:04:32.135804');
-
-
-INSERT INTO reservation_details("reservation_id","reservation_date","site_rate","tax_rate","rate_type_name","created_at")
-VALUES
-(9,E'2021-08-31',6000,0.1,E'ハイシーズン料金',E'2021-08-10 00:02:39.238429'),
-(9,E'2021-09-01',3000,0.1,E'通常料金',E'2021-08-10 00:02:39.238429'),
-(10,E'2021-09-03',5000,0.1,E'通常料金',E'2021-08-10 00:02:53.467315'),
-(11,E'2021-08-31',9000,0.1,E'ハイシーズン料金',E'2021-08-10 00:03:18.611791'),
-(11,E'2021-09-01',6000,0.1,E'通常料金',E'2021-08-10 00:03:18.611791'),
-(12,E'2021-09-09',3000,0.1,E'通常料金',E'2021-08-10 00:03:28.049619'),
-(13,E'2021-08-12',8000,0.1,E'ハイシーズン料金',E'2021-08-10 00:03:56.011949'),
-(14,E'2021-08-13',9000,0.1,E'ハイシーズン料金',E'2021-08-10 00:04:04.620321'),
-(15,E'2021-09-09',6000,0.1,E'通常料金',E'2021-08-10 00:04:32.135804'),
-(15,E'2021-09-10',6000,0.1,E'通常料金',E'2021-08-10 00:04:32.135804');
+(1,E'2021-09-02',4000,0.1,E'ハイシーズン料金',E'2021-08-15 13:45:31.934168'),
+(1,E'2021-09-03',4000,0.1,E'ハイシーズン料金',E'2021-08-15 13:45:31.934168'),
+(2,E'2021-08-25',7000,0.1,E'ハイシーズン料金',E'2021-08-15 13:46:07.733142'),
+(3,E'2021-08-31',7000,0.1,E'ハイシーズン料金',E'2021-08-15 13:46:33.467437'),
+(4,E'2021-09-15',5500,0.1,E'ハイシーズン料金',E'2021-08-15 13:48:15.109568'),
+(4,E'2021-09-16',5500,0.1,E'ハイシーズン料金',E'2021-08-15 13:48:15.109568'),
+(4,E'2021-09-17',5500,0.1,E'ハイシーズン料金',E'2021-08-15 13:48:15.109568'),
+(5,E'2021-09-13',7000,0.1,E'ハイシーズン料金',E'2021-08-15 13:48:50.726346'),
+(6,E'2021-09-18',5500,0.1,E'ハイシーズン料金',E'2021-08-15 13:49:31.789736'),
+(6,E'2021-09-19',5500,0.1,E'ハイシーズン料金',E'2021-08-15 13:49:31.789736'),
+(7,E'2021-08-28',7000,0.1,E'ハイシーズン料金',E'2021-08-15 13:50:34.095119');
